98177305d4950ccc44f62c6ecfe53fd870b91155
Lmagic16 <1014684123@qq.com>
2017-06-17 21:20:17 +0800

    【增加】运动总结页面

--- pages/summary/summary.js
+++ pages/summary/summary.js
@@ -1,70 +1,139 @@
-// summary.js
-
-var data = require('../../utils/data.js')
-var wxCharts = require('../../utils/wxcharts.js')
-
+//summary.js
+var wxCharts = require('../../utils/wxcharts.js');
+var app = getApp();
+var lineChart = null;
+var ringChart = null;
 Page({
+    data: {
+    },
+    touchHandler: function (e) {
+        console.log(lineChart.getCurrentDataIndex(e));
+        lineChart.showToolTip(e, {
+             background: '#7cb5ec'
+        });
+    }, /*
+    createSimulationData: function () {
+        var categories = [];
+        var data = [];
+        for (var i = 0; i < 10; i++) {
+            categories.push('2016-' + (i + 1));
+            data.push(Math.random()*(20-10)+10);
+        }
+        // data[4] = null;
+        return {
+            categories: categories,
+            data: data
+        }
+    },
+    updateData: function () {
+        var simulationData = this.createSimulationData();
+        var series = [{
+            name: '成交量1',
+            data: simulationData.data,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        }];
+        lineChart.updateData({
+            categories: simulationData.categories,
+            series: series
+        });
+    },*/
+    onLoad: function (e) {
+        var windowWidth = 320;
+        try {
+            var res = wx.getSystemInfoSync();
+            windowWidth = res.windowWidth;
+        } catch (e) {
+            console.error('getSystemInfoSync failed!');
+        }
+        
+        /*var simulationData = this.createSimulationData();*/
+        lineChart = new wxCharts({
+            canvasId: 'heartRateCanvas',
+            type: 'line',
+            categories: ['12:40','12:42','12:44','12:46','12:48','12:50','12:52','12:54','12:56','12:58','13:00'],
+            /*categories: simulationData.categories,*/
+            animation: true,
+            background: '#f5f5f5',
+            series: [/*{
+                name: '成交量1',
+                data: simulationData.data,
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            },*/ {
+                name: '运动心率轨迹',
+                data: [80, 110, 140, 160, 155, 140, 150, 162, 171, 179, 120],
+                format: function (val) {
+                    return val;
+                }
+            }],
+            xAxis: {
+                disableGrid: true
+            },
+            yAxis: {
+                title: '运动心率',
+                format: function (val) {
+                    return val+ 'bpm';
+                },
+                min: 60,
+                max: 200,
+                gridColor:["#FF0000","#CC6600","#009966","#FF0000"]
+            },
+            width: windowWidth,
+            height: 200,
+            dataLabel: true,
+            dataPointShape: true,
+            extra: {
+                lineStyle: 'curve'
+            }
+        });
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      summaryData : data.getSummaryData(),
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+        ringChart = new wxCharts({
+            animation: true,
+            canvasId: 'ringCanvas',
+            type: 'ring',
+            extra: {
+                ringWidth: 25
+            },
+            title: {
+                name: '运动区间占比',
+                color: '#7cb5ec',
+                fontSize: 15,
+                offsetX: 0
+            },
+            subtitle: {
+                name: '收益率',
+                color: '#666666',
+                fontSize: 15
+            },
+            series: [{
+                name: '成交量1',
+                data: 30,
+                stroke: false,
+                color:"#009966"
+            }, {
+                name: '成交量2',
+                data: 55,
+                 stroke: false
+            }, {
+                name: '成交量3',
+                data: 20,
+                stroke: false
+            }],
+            disablePieStroke: true,
+            width: windowWidth,
+            height: 200,
+            dataLabel: false,
+            legend: false,
+            padding: 0
+        });
+        ringChart.addEventListener('renderComplete', () => {
+            console.log('renderComplete');
+        });
+        setTimeout(() => {
+            ringChart.stopAnimation();
+        }, 500);
+    }
+});
